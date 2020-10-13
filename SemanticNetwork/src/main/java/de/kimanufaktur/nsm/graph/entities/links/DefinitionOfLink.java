c3682ffb23febc036c8f76861c2a8883b8459895
Hannes Kanthak <hannes.kanthak@implisense.com>
2020-10-13 13:33:26 +0200

    add links

---
+++ SemanticNetwork/src/main/java/de/kimanufaktur/nsm/graph/entities/links/DefinitionOfLink.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) Johannes Fähndrich - All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly
+ * prohibited Proprietary and confidential.
+ * Written by Johannes Fähndrich <faehndrich@gmail.com.com>,  2011
+ */
+
+package de.kimanufaktur.nsm.graph.entities.links;
+
+/**
+ * Created by faehndrich on 29.07.15.
+ */
+public class DefinitionOfLink extends WeightedLink {}
---
+++ SemanticNetwork/src/main/java/de/kimanufaktur/nsm/graph/entities/links/HolonymLink.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) Johannes Fähndrich - All Rights Reserved.
+ * Unauthorized copying of this file, via any medium is strictly
+ * prohibited Proprietary and confidential.
+ * Written by Johannes Fähndrich <faehndrich@gmail.com.com>,  2011
+ */
+
+package de.kimanufaktur.nsm.graph.entities.links;
+
+public class HolonymLink extends WeightedLink {}
